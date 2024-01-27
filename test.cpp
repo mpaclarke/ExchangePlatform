fb1fd2adacfdc902ba99ce7ddea4ab8a8b64eb82
Michael Clarke <mpaclarke@gmail.com>
2024-01-27 19:02:02 +0000

    add course 3 week 1 work to the repo

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 std::vector<std::string> tokenise(std::string csvLine, char separator)
 {
@@ -34,14 +35,64 @@ std::vector<std::string> tokenise(std::string csvLine, char separator)
 
 int main()
 {
+    // std::vector<std::string> tokens;
+    // std::string s = "2020/03/17 17:01:24.884492,ETH/BTC,bid,0.02187308,7.44564869";
+
+    // tokens = tokenise(s, ',');
+
+    // for (const std::string& t : tokens)
+    // {
+    //     std::cout << t << std::endl;
+    // }
+
+    std::ifstream csvFile{"20200317.csv"};
+    std::string line;
     std::vector<std::string> tokens;
-    std::string s = "2020/03/17 17:01:24.884492,ETH/BTC,bid,0.02187308,7.44564869";
 
-    tokens = tokenise(s, ',');
-
-    for (const std::string& t : tokens) 
+    int lineCount = 1;
+    if (csvFile.is_open())
     {
-        std::cout << t << std::endl;
+        std::cout << "File open " << std::endl;
+
+        while (std::getline(csvFile, line))
+        {
+            std::cout << "Read line " << line << std::endl;
+            tokens = tokenise(line, ',');
+            if (tokens.size() != 5)
+            {
+                std::cout << "BAD LINE" << std::endl;
+                continue;
+            }
+
+            try
+            {
+                double price = std::stod(tokens[3]);
+                double amount = std::stod(tokens[4]);
+                std::cout << price << " : " << amount << std::endl;
+            }
+            catch (const std::exception &e)
+            {
+                std::cout << "COULD NOT READ LINE NUMBER " << lineCount << std::endl;
+                std::cout << "BAD FLOAT: " << tokens[3] << std::endl;
+                std::cout << "BAD FLOAT: " << tokens[4] << std::endl;
+                continue;
+                //std::cerr << e.what() << '\n';
+            }
+
+            ++lineCount;
+        }
+
+        csvFile.close();
+        if (!csvFile.is_open())
+        {
+            line.clear();
+            std::cout << "The flie is closed." << line << std::endl;
+        }
+        std::cout << "Read " << lineCount << " lines from the file." << std::endl;
+    }
+    else
+    {
+        std::cout << "Could not open file. " << std::endl;
     }
 
     return 0;
